4eb0c111f80798e216225f007f417a7068c70e1f
shiveshdixit99 <48621843+shiveshdixit99@users.noreply.github.com>
2022-05-02 00:24:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const Order = require('./models/order');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+app.get('/orders', async (req, res) => {
+    const orders = await Order.find({});
+    //console.log(orders);
+    //res.send("recieved");
+    res.render('orders/index', { orders })
+});
+app.get('/orders/new', (req, res) => {
+    res.render('orders/new');
+})
+
+app.post('/orders', async (req, res) => {
+    const order = new Order(req.body.order);
+    //res.send(order);
+    await order.save();
+    res.redirect(`/orders/${order._id}`)
+})
+
+app.get('/orders/:id', async (req, res,) => {
+    const order = await Order.findById(req.params.id)
+    res.render('orders/show', { order });
+});
+
+app.get('/orders/:id/edit', async (req, res) => {
+    // res.send("HI");
+    const order = await Order.findById(req.params.id)
+    //res.send(order);
+    res.render('orders/edit', { order });
+})
+
+app.put('/orders/:id', async (req, res) => {
+    const { id } = req.params;
+    const order = await Order.findByIdAndUpdate(id, { ...req.body.order });
+    res.redirect(`/orders/${order._id}`)
+});
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
